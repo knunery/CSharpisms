05cd11c96c79531336684eb3230ecb3115339dc8
Kyle Nunery <kyle.nunery@gmail.com>
2013-12-22 15:08:58 -0600

    Create PreJit.cs

---
+++ src/PreJit.cs
@@ -0,0 +1,17 @@
+// http://www.codeproject.com/Articles/31316/Pre-compile-pre-JIT-your-assembly-on-the-fly-or-tr
+Thread jitter = new Thread(() =>
+{
+  foreach (var type in Assembly.Load("MyHavyAssembly, Version=1.8.2008.8," + 
+           " Culture=neutral, PublicKeyToken=8744b20f8da049e3").GetTypes())
+  {
+    foreach (var method in type.GetMethods(BindingFlags.DeclaredOnly | 
+                        BindingFlags.NonPublic | 
+                        BindingFlags.Public | BindingFlags.Instance | 
+                        BindingFlags.Static))
+    {
+      System.Runtime.CompilerServices.RuntimeHelpers.PrepareMethod(method.MethodHandle);
+    }
+  }
+});
+jitter.Priority = ThreadPriority.Lowest;
+jitter.Start();
